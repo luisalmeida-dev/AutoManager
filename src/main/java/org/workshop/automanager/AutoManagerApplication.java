735042b17398a8ead4af5aac6641fc3f580696b0
Luis Almeida <anjos.luiseduardo@gmail.com>
2024-12-19 19:05:07 -0300

    first commit

---
+++ src/main/java/org/workshop/automanager/AutoManagerApplication.java
@@ -0,0 +1,13 @@
+package org.workshop.automanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/workshop/automanager/AutoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.workshop.automanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
